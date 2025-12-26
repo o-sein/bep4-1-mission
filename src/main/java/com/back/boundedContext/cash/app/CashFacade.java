ac0b79c1c9b4077cd5f700710d5c918ff909834b
ooooo_ori <dhtp0112@gmail.com>
2025-12-26 16:10:13 +0900

    0026(fix) : CashSupport 변경 사항 누락

--- src/main/java/com/back/boundedContext/cash/app/CashFacade.java
+++ src/main/java/com/back/boundedContext/cash/app/CashFacade.java
@@ -2,8 +2,6 @@ package com.back.boundedContext.cash.app;
 
 import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.domain.Wallet;
-import com.back.boundedContext.cash.out.CashMemberRepository;
-import com.back.boundedContext.cash.out.WalletRepository;
 import com.back.shared.cash.dto.CashMemberDto;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +13,9 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class CashFacade {
-    private final CashMemberRepository cashMemberRepository;
-    private final WalletRepository walletRepository;
-    private final CashCreateWalletUseCase cashCreateWalletUseCase;
+    private final CashSupport cashSupport;
     private final CashSyncMemberUseCase cashSyncMemberUseCase;
+    private final CashCreateWalletUseCase cashCreateWalletUseCase;
 
     @Transactional
     public CashMember syncMember(MemberDto member) {
@@ -32,11 +29,11 @@ public class CashFacade {
 
     @Transactional(readOnly = true)
     public Optional<CashMember> findMemberByUsername(String username) {
-        return cashMemberRepository.findByUsername(username);
+        return cashSupport.findMemberByUsername(username);
     }
 
     @Transactional(readOnly = true)
     public Optional<Wallet> findWalletByHolder(CashMember holder) {
-        return walletRepository.findByHolder(holder);
+        return cashSupport.findWalletByHolder(holder);
     }
 }
