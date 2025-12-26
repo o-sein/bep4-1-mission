a1592980ec025af92521beb84fc2ebbdf8c97fde
ooooo_ori <dhtp0112@gmail.com>
2025-12-26 15:03:33 +0900

    0026 : cash 모듈 리팩토링, Facade에 직접 구현된 기능들을 유즈케이스로 분리

--- src/main/java/com/back/boundedContext/cash/app/CashSupport.java
+++ src/main/java/com/back/boundedContext/cash/app/CashSupport.java
@@ -4,37 +4,21 @@ import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.domain.Wallet;
 import com.back.boundedContext.cash.out.CashMemberRepository;
 import com.back.boundedContext.cash.out.WalletRepository;
-import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Service
+@Component
 @RequiredArgsConstructor
-public class CashFacade {
+public class CashSupport {
     private final CashMemberRepository cashMemberRepository;
     private final WalletRepository walletRepository;
-    private final CashCreateWalletUseCase cashCreateWalletUseCase;
-    private final CashSyncMemberUseCase cashSyncMemberUseCase;
 
-    @Transactional
-    public CashMember syncMember(MemberDto member) {
-        return cashSyncMemberUseCase.syncMember(member);
-    }
-
-    @Transactional
-    public Wallet createWallet(CashMember holder) {
-        return cashCreateWalletUseCase.createWallet(holder);
-    }
-
-    @Transactional(readOnly = true)
     public Optional<CashMember> findMemberByUsername(String username) {
         return cashMemberRepository.findByUsername(username);
     }
 
-    @Transactional(readOnly = true)
     public Optional<Wallet> findWalletByHolder(CashMember holder) {
         return walletRepository.findByHolder(holder);
     }
